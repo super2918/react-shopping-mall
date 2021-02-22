8c09d67f1389d8013cb0bcd574edf07b78de0d1b
mc2918 <tlsdud1375@gmail.com>
2021-02-22 19:30:00 +0900

    [Add] - 메인 배너, 상품 컨텐츠, 푸터 추가

--- src/App.js
+++ src/App.js
@@ -1,27 +1,65 @@
 
+import { useState } from 'react';
+import { Form, Navbar, Nav, Button, FormControl, Jumbotron } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Data from './data';
 import './App.css';
-import { Navbar, Form, Nav, Button, FormControl, } from 'react-bootstrap';
+
 
 function App() {
+  let [ shoes, setShoes ] = useState(Data);
+
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link1">Men</Nav.Link>
+            <Nav.Link href="#link2">Women</Nav.Link>
+            <Nav.Link href="#link3">Kids</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">검색</Button>
+          </Form>
+        </Navbar.Collapse>
       </Navbar>
 
-      
-      
+      <Jumbotron className="bg-main" fluid>
+        <h1>20% Season OFF</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
+          </p>
+        <div>
+          <Button variant="primary">Learn more</Button>
+        </div>
+      </Jumbotron>
+
+      <main>
+        <div className="row">
+          <Card />
+        </div>
+      </main>
+
+      <Footer />
+
     </div>
   );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes1.jpg'} alt="" width="100%"/>
+      <h4>{props.shoes[0].title}</h4>
+      <p>{props.shoes[1].content}</p>
+      <p>{props.shoes[2].price}</p>
+    </div>
+  )
 }
 
 
