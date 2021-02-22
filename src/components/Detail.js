1185550adb7094e325ed497752dc918c89185ba5
mc2918 <tlsdud1375@gmail.com>
2021-02-23 01:00:49 +0900

    [Add] - Detail 상세페이지 추가

---
+++ src/components/Detail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function Detail(props) {
+
+  // const { productId } = useParams(); 
+
+  return (
+    <div>페이지 생성중</div>
+  )
+}
+
+export default Detail;
