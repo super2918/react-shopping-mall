d9c5c7e03a49c9f20d01eb0707d6b501d8b04d71
mc2918 <tlsdud1375@gmail.com>
2021-02-23 19:15:00 +0900

    [Add] - Cart 컴포넌트 추가

---
+++ src/components/Cart.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import './../scss/contents/Cart.scss';
+
+function Cart(props) {
+  return (
+    <div className="container">
+      <div className="cart">
+        <h2 className="cart__title">장바구니</h2>
+
+        <ul className="cart__notice">
+          <li>* 장바구니 상품은 최대 30일간 저장됩니다.</li>
+          <li>* 가격, 옵션 등 정보가 변경된 경우 주문이 불가할 수 있습니다.</li>
+          <li>* 오늘출발 상품은 판매자 설정 시점에 따라 오늘출발 여부가 변경될 수 있으니 주문 시 꼭 다시 확인해 주시기 바랍니다.</li>
+        </ul>
+
+        <div className="cart__list">
+          <Table responsive className="cart__table">
+            <thead>
+              <tr>
+                <th></th>
+                <th>상품정보</th>
+                <th>수량</th>
+                <th>상품비</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                props.state.map((n, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{ n.id++ }</td>
+                      <td>{ n.name }</td>
+                      <td>{ n.quan }</td>
+                      <td>{ n.price }</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function stateToProps(state) {
+  return {
+    state: state
+  }
+}
+
+export default connect(stateToProps)(Cart);
