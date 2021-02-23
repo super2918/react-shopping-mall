a9d447a247cad84bd9835c6f22d27713d275974e
mc2918 <tlsdud1375@gmail.com>
2021-02-24 00:41:50 +0900

    [Add] - 리덕스 더미 데이터 추가

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+let store = createStore(() => {
+  return [
+    { id: 0, name: '줌X 인빈서블런 플라이니트', quan: 1, price: '200000'},
+    { id: 1, name: '줌X 인빈서블런 2', quan: 2, price: '230000'},
+    { id: 2, name: '줌X 인빈서블런 플라이니트4', quan: 1, price: '240000'},
+  ]
+ })
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter basename="">
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
