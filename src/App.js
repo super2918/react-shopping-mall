b4f717be7afc062e2b4ca37e58547c97b2217632
mc2918 <tlsdud1375@gmail.com>
2021-02-24 00:39:23 +0900

    [Add] - 잔여수량 부분 추가, 더보기 버튼 수정

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@
 import { useState } from 'react';
 import { Navbar, Nav, Button,Jumbotron } from 'react-bootstrap';
 import { Switch, Link, Route, useHistory } from 'react-router-dom';
-import Footer from './components/Footer';
 import Detail from './components/Detail';
+import Cart from './components/Cart';
+import styled from 'styled-components';
 import Data from './data';
 import axios from 'axios';
 
 import './App.css';
 
+let StockInfoText = styled.span`
+  > strong {
+    padding-right: 10px;
+  }
+`;
+
 function App() {
   let [ product, setProduct ] = useState(Data);
+  let [ stock, setStock ] = useState([10, 5, 20, 30, 50, 1]);
 
   return (
     <div className="App">
@@ -18,10 +26,10 @@ function App() {
         <Navbar.Brand><Nav.Link as={Link} to='/'>LOGO</Nav.Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="ml-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/detail/1">Detail</Nav.Link>
-            <Nav.Link as={Link} to="/detail/2">Detail2</Nav.Link>
+            <Nav.Link as={Link} to="/detail/0">상세보기</Nav.Link>
+            <Nav.Link as={Link} to="/cart">장바구니</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
@@ -45,14 +53,17 @@ function App() {
               <div className="row">
                 {
                   product.map((product, i) => {
-                    return <Card product={product} index={i} key={i} />
+                    return <Card product={product} stock={ stock } index={i} key={i} />
                   }) 
                 }
               </div>
-              <button type="button" className="btn btn-primary mt-5 mb-5" onClick={() => {
+              <button type="button" className="btn btn-primary mt-5 mb-5 btn-more" onClick={() => {
                 axios.get('https://codingapple1.github.io/shop/data2.json')
                 .then((response) => {
                   setProduct([...product, ...response.data])
+                  
+                   const button =  document.querySelector('.btn-more');
+                   button.style.display = 'none';
                 })
                 .catch((error) => {
                   console.log('실패');
@@ -66,8 +77,11 @@ function App() {
           <Detail product={ product } />
         </Route>
 
+        <Route path="/cart">
+          <Cart  />
+        </Route>
+
       </Switch>
-      <Footer />
 
     </div>
   );
@@ -80,10 +94,20 @@ function Card(props) {
   return (
     <div className="col-md-4" onClick={() => { history.push('/detail/' + props.product.id ) }}>
       <img src={'https://codingapple1.github.io/shop/shoes'+( props.index + 1) +'.jpg'} alt="신발 제품 이미지" width="100%"/>
-      <h4>{props.product.title}</h4>
-      <p>{props.product.content}</p>
-      <p>{props.product.price}</p>
+      <h4>{ props.product.title }</h4>
+      <p>{ props.product.content }</p>
+      <p>{ props.product.price }</p>
+      <p><StockInfo stock={props.stock} index={props.index} /></p>
     </div>
+  )    
+}
+
+function StockInfo(props) {
+  return (
+    <StockInfoText>
+      <strong>잔여수량</strong>
+      <span>{props.stock[props.index]}</span>
+    </StockInfoText>
   )
 }
 
